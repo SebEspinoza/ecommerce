5f563c929de5e6a2357c6e98cde3e1e445c46fe3
SebEspinoza <seba.espinozal@duocuc.cl>
2022-07-11 23:19:29 -0400

    added products page and footer banner

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -3,10 +3,26 @@ import Link from 'next/link'
 
 import { urlFor } from '../lib/client'
 
-const FooterBanner = ({ footerBanner }) => {
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, product, buttonText, image, desc } }) => {
     return (
         <div className='footer-banner-container'>
-            <div className=''></div>
+            <div className='banner-desc'>
+                <div className='left'>
+                    <p>{discount}</p>
+                    <h3>{largeText1}</h3>
+                    <h3>{largeText2}</h3>
+                    <p>{saleTime}</p>
+                </div>
+                <img src={urlFor(image)} width={300} height={280} classname="footer-banner-image" />
+                <div className='right'>
+                    <p>{smallText}</p>
+                    <h3>{midText}</h3>
+                    <p>{desc}</p>
+                    <Link href={`/product/${product}`}>
+                        <button type='button'>{buttonText}</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
